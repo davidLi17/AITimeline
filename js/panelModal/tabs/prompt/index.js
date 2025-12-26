b50d7d93a47c97925595eea210ad46fbe95f2751
侯衍超 <houyanchao@outlook.com>
2025-12-26 22:55:58 +0800

    no message

--- js/panelModal/tabs/prompt/index.js
+++ js/panelModal/tabs/prompt/index.js
@@ -11,10 +11,8 @@ class PromptTab extends BaseTab {
         super();
         this.id = 'prompt';
         this.name = chrome.i18n.getMessage('hosegod');
-        this.icon = `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
-            <line x1="9" y1="9" x2="15" y2="9"/>
-            <line x1="12" y1="6" x2="12" y2="12"/>
+        this.icon = `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/>
         </svg>`;
     }
     
